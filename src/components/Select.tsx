ea52e3618fee2eca2ccc854afcb7fa9692e6136b
romakh-i <ivanromah@gmail.com>
2019-02-22 18:48:56 +0200

    select

--- src/components/Select.tsx
+++ src/components/Select.tsx
@@ -1,17 +1,28 @@
 import * as React from 'react'
 
-class Select extends React.Component {
+
+class Select extends React.Component<{}, { display: string }> {
+  constructor(props: any) {
+    super(props);
+
+    this.state = { display: 'none' }
+  }
+
+  public dropdownClick = (e: any) => {
+    e.preventDefault();
+    this.setState({ display: 'block' });
+  }
   public render() {
+    const selectStyle = {
+      display: this.state.display
+    }
+
     return (
-      <div className="select">
-        <select required={true}>
-          <option>sdfsd</option>
-          <option>qwer</option>
-          <option>s q q</option>
-        </select>
+      <div>
+        <div className="select" onClick={this.dropdownClick}>Category</div>
         {/* <button className="select-btn bg-white dropdown-toggle" type="button" id="dropdownCategory">Category</button> */}
 
-        <div className="select-menu" style={{ display: 'none' }}>
+        <div className="select-menu" style={selectStyle}>
           <button className="select-item" type="button">Action</button>
           <button className="select-item" type="button">Another action</button>
           <button className="select-item" type="button">Something else here</button>
